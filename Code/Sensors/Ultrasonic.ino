ebe3118d826614b7ebf80b29870175635f90d35d
farialucas <lucasaafaria@gmail.com>
2018-03-07 19:42:12 -0300

    Comments added

--- Code/Sensors/Ultrasonic.ino
+++ Code/Sensors/Ultrasonic.ino
@@ -1,8 +1,10 @@
+/*--------------------------- Ultrasonic Sensor Reading --------------------------*/
+
 #include <NewPing.h>
 
 #define PIN_TRIG 4
 #define PIN_ECHO 5
-#define MAX_DIST 200
+#define MAX_DIST 200	// Distance reading functions return 0 if the read value is greater than this one
  
 NewPing us(PIN_TRIG, PIN_ECHO, MAX_DIST);
 
@@ -10,11 +12,10 @@ float dist;
 
 void setup() {
   Serial.begin(9600);
-  Serial.println("Lendo dados do sensor...");
 }
  
 void loop() {
-  dist = us.convert_cm(us.ping_median(5));
+  dist = us.convert_cm(us.ping_median(5)); // Takes the median of 5 values (more accurate)	
   Serial.print("\nDistancia em cm: ");
   Serial.print(dist);
   delay(500);
