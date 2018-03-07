464ffa2697ee5f9129fc1694b1c6ae641d57ab98
lucasaafaria <31375155+lucasaafaria@users.noreply.github.com>
2018-03-07 09:25:35 -0300

    example of code pattern

---
+++ Code/CodeTemplate.c
@@ -0,0 +1,29 @@
+#include <huehue.h>
+
+#define DIGO 10
+#define LUCAS 12
+
+typedef struct {
+	int mechanic;
+	int electronics;
+	int prog;
+} competition_t;
+
+void MyFunction(competition_t competition){
+	printf("Coe Rapaziada");
+}
+
+int main(){
+	const int VANESSA = 4;
+	int rebeca = 25;
+	int miniBau = 72;
+	competition_t fireFighting;
+
+	fireFighting.mechanic = DIGO + VANESSA;
+	fireFighting.electronics = rebeca + miniBau;
+	fireFighting.prog = LUCAS;
+  
+	MyFunction();
+  
+	return 0;
+}
