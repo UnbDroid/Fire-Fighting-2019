015d7bb24b1562598134a99f4baff1d0c7c96ea7
farialucas <lucasaafaria@gmail.com>
2018-04-04 06:51:44 -0300

    added pump code

---
+++ Code/Pump/pump.ino
@@ -0,0 +1,21 @@
+/*--------------------------- Water Pump Usage --------------------------*/
+
+#define PUMP 8
+ 
+void onPump(){
+	digitalWrite(PUMP, HIGH);
+}
+
+void offPump(){
+	digitalWrite(PUMP, LOW);
+}
+
+void setup() {
+  pinMode(PUMP, OUTPUT);
+}
+ 
+void loop() {
+	onPump();
+	delay(1000);
+	offPump(3000);
+}
