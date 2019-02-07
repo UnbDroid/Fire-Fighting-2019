a9fc3ae65ea276df54706a2456951286ae2f6b35
Pedro <pedrosaman96@gmail.com>
2019-02-07 15:12:03 -0200

    Setup US

--- Code/Sensors/Ultrasonic.ino
+++ Code/Sensors/Ultrasonic.ino
@@ -3,11 +3,23 @@
 
 #include <NewPing.h>
 
-#define PIN_TRIG 4
-#define PIN_ECHO 5
+#define PIN_ECHO_1 53
+#define PIN_TRIG_1 52
+#define PIN_ECHO_2 51
+#define PIN_TRIG_2 50
+#define PIN_ECHO_3 49
+#define PIN_TRIG_3 48
+#define PIN_ECHO_4 47
+#define PIN_TRIG_4 46
+#define PIN_ECHO_5 45
+#define PIN_TRIG_5 44 
 #define MAX_DIST 200	// Distance reading functions return 0 if the read value is greater than this one
  
-NewPing us(PIN_TRIG, PIN_ECHO, MAX_DIST);
+NewPing us_frontal(PIN_TRIG_1, PIN_ECHO_1, MAX_DIST);
+NewPing us_left_front(PIN_TRIG_1, PIN_ECHO_1, MAX_DIST);
+NewPing us_left_back(PIN_TRIG_1, PIN_ECHO_1, MAX_DIST);
+NewPing us_right_front(PIN_TRIG_1, PIN_ECHO_1, MAX_DIST);
+NewPing us_right_back(PIN_TRIG_1, PIN_ECHO_1, MAX_DIST);
 
 float dist;
 
@@ -16,7 +28,7 @@ void setup() {
 }
  
 void loop() {
-  dist = us.convert_cm(us.ping_median(5)); // Takes the median of 5 values (more accurate)	
+  dist = us_frontal.convert_cm(us.ping_median(5)); // Takes the median of 5 values (more accurate)	
   Serial.print("\nDistancia em cm: ");
   Serial.print(dist);
   delay(500);
