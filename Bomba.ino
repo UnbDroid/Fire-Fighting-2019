00b21e8b6be44e08dbab329abbb774930ade00b0
rebecahss <31423619+rebecahss@users.noreply.github.com>
2019-02-07 16:41:56 -0200

    Add files via upload

---
+++ Bomba.ino
@@ -0,0 +1,21 @@
+int a=1;
+const int bombaPin =  5;  
+const int Chama = 4;
+ 
+void setup() {
+  
+  Serial.begin(9600);
+  pinMode(bombaPin, OUTPUT);
+  pinMode(Chama, OUTPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  while(true){
+
+ while(digitalRead(Chama)== HIGH){
+    digitalWrite(bombaPin, HIGH);
+    }
+    digitalWrite(bombaPin, LOW);
+  
+}
