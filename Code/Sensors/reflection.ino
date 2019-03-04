dcdea32a82f6ee916ea85f0efd8fd0e5e43b06d3
Lucas Faria <lucasaafaria@gmail.com>
2019-03-04 13:11:29 -0300

    Created lib for reflection sensor

--- Code/Sensors/reflection.ino
+++ Code/Sensors/reflection.ino
@@ -1,22 +1,15 @@
-#define ANALOG_REF  A8
-#define DIGITAL_REF 8
+#include "reflection/reflection.h"
+
+#define COLOR_PIN 8
+
+// Reflection object declaring. Receives digital pin number for the reflection sensor.
+Reflection color_sensor(COLOR_PIN); 
 
 void setup() {
-    pinMode(ANALOG_REF, INPUT);
-    pinMode(DIGITAL_REF, INPUT);
     Serial.begin(9650);
 }
 
-int analog;
-bool digital;
-
-void readColor() {
-    analog = analogRead(ANALOG_REF);
-    digital = digitalRead(DIGITAL_REF);
-    Serial.print(analog);
-    Serial.print(" ");
-    Serial.println(digital);
-}
 void loop() {
-    readColor();
+    color_sensor.update(); // reads the sensor
+    Serial.println(color_sensor.getColor()); // prints the read value
 }
